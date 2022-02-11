e89ae830b7547ea8d7e8efaa2366f651bb17f2d5
Fikcup <rhys.n.wright@gmail.com>
2022-02-10 17:49:39 -0800

    feat : cart model

---
+++ common/src/models/Cart.ts
@@ -0,0 +1,22 @@
+import { getModelForClass, prop, Ref } from '@typegoose/typegoose';
+import mongoose from 'mongoose';
+import { User } from './User';
+
+export class Cart {
+    @prop({ 
+        required: true,
+        default: new mongoose.Types.ObjectId() 
+    })
+    public id!: mongoose.Types.ObjectId;
+
+    @prop({
+        required: true,
+        ref: () => User
+    })
+    public user!: Ref<User>;
+}
+
+export const CartModel = getModelForClass(Cart);
+
+let document = async () => await CartModel.create({ name: 'Cart' });
+document();
