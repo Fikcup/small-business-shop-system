eedb17033c59aa0d6f13283d0fc70ddee0194ef5
Fikcup <rhys.n.wright@gmail.com>
2022-02-10 17:56:16 -0800

    feat : shop model

---
+++ common/src/models/Shop.ts
@@ -0,0 +1,41 @@
+import { getModelForClass, prop, Ref } from '@typegoose/typegoose';
+import mongoose from 'mongoose';
+import { User } from './User';
+
+export class Shop {
+    @prop({ 
+        required: true,
+        default: new mongoose.Types.ObjectId() 
+    })
+    public id!: mongoose.Types.ObjectId;
+
+    @prop({ 
+        required: true, 
+        default: true
+    })
+    public active!: boolean;
+
+    @prop({
+        required: true
+    })
+    public name!: string;
+
+    @prop()
+    public description?: string;
+
+    @prop({ 
+        required: true, 
+        default: 'https://imgur.com/0RpFv3U'
+    })
+    public img!: URL;
+
+    @prop({
+        ref: () => User
+    })
+    protected addressArr?: Ref<User>;
+}
+
+export const ShopModel = getModelForClass(Shop);
+
+let document = async () => await ShopModel.create({ name: 'Shop' });
+document();
