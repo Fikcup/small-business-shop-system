704f619a4d1fe2d517998fc025cbe93a031a3940
Fikcup <rhys.n.wright@gmail.com>
2022-02-19 12:43:28 -0800

    fix : bug fix circular validation error

--- common/dist/index.d.ts
+++ common/dist/index.d.ts
@@ -1,6 +1,2 @@
 export * from './models/User';
-export * from './models/Shop';
-export * from './models/Cart';
-export * from './models/Product';
-export * from './models/Order';
 export * from './models/Address';
--- common/dist/index.js
+++ common/dist/index.js
@@ -11,8 +11,8 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./models/User"), exports);
-__exportStar(require("./models/Shop"), exports);
-__exportStar(require("./models/Cart"), exports);
-__exportStar(require("./models/Product"), exports);
-__exportStar(require("./models/Order"), exports);
+// export * from './models/Shop';
+// export * from './models/Cart';
+// export * from './models/Product';
+// export * from './models/Order';
 __exportStar(require("./models/Address"), exports);
