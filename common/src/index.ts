5ef9f6774f7b8c963326211dfa0276ecd837efb5
Fikcup <rhys.n.wright@gmail.com>
2022-02-19 12:41:32 -0800

    fix : circular validation error

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,6 +1,6 @@
-export * from './models/Address';
 export * from './models/User';
 export * from './models/Shop';
 export * from './models/Cart';
 export * from './models/Product';
 export * from './models/Order';
+export * from './models/Address';
